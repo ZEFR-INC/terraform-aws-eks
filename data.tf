f0dec2413f0efecadcd7c157129fce60ed02aa66
Trevor Carlson <trevor.carlson@zefr.com>
2019-08-05 17:11:02 -0700

    fix tags, name prefix, and ami filter

--- data.tf
+++ data.tf
@@ -18,13 +18,13 @@ data "aws_iam_policy_document" "workers_assume_role_policy" {
 data "aws_ami" "eks_worker" {
   filter {
     name   = "name"
-    values = ["amazon-eks-node-${var.cluster_version}-${var.worker_ami_name_filter}"]
+    values = ["${var.worker_ami_name_filter}"]
   }
 
   most_recent = true
 
   # Owner ID of AWS EKS team
-  owners = ["602401143452"]
+  owners = ["363848507644"]
 }
 
 data "aws_iam_policy_document" "cluster_assume_role_policy" {
--- workers_launch_template.tf
+++ workers_launch_template.tf
@@ -2,7 +2,7 @@
 
 resource "aws_autoscaling_group" "workers_launch_template" {
   count                   = "${var.worker_group_launch_template_count}"
-  name_prefix             = "${aws_eks_cluster.this.name}-${lookup(var.worker_groups_launch_template[count.index], "name", count.index)}"
+  name_prefix             = "${lookup(var.worker_groups_launch_template[count.index], "name", count.index)}"
   desired_capacity        = "${lookup(var.worker_groups_launch_template[count.index], "asg_desired_capacity", local.workers_group_defaults["asg_desired_capacity"])}"
   max_size                = "${lookup(var.worker_groups_launch_template[count.index], "asg_max_size", local.workers_group_defaults["asg_max_size"])}"
   min_size                = "${lookup(var.worker_groups_launch_template[count.index], "asg_min_size", local.workers_group_defaults["asg_min_size"])}"
@@ -22,7 +22,7 @@ resource "aws_autoscaling_group" "workers_launch_template" {
 
   tags = ["${concat(
     list(
-      map("key", "Name", "value", "${aws_eks_cluster.this.name}-${lookup(var.worker_groups_launch_template[count.index], "name", count.index)}-eks_asg", "propagate_at_launch", true),
+      map("key", "Name", "value", "${lookup(var.worker_groups_launch_template[count.index], "name", count.index)}", "propagate_at_launch", true),
       map("key", "kubernetes.io/cluster/${aws_eks_cluster.this.name}", "value", "owned", "propagate_at_launch", true),
       map("key", "k8s.io/cluster-autoscaler/${lookup(var.worker_groups_launch_template[count.index], "autoscaling_enabled", local.workers_group_defaults["autoscaling_enabled"]) == 1 ? "enabled" : "disabled"  }", "value", "true", "propagate_at_launch", false),
       map("key", "k8s.io/cluster-autoscaler/${aws_eks_cluster.this.name}", "value", "", "propagate_at_launch", false),
